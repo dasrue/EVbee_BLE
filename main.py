e51c873602ab0f4b68c4cfe2706b593bc6716242
Brad Lonergan <email@bradlonergan.co.nz>
2024-02-05 17:36:35 +1300

    Added some more command handlers

--- main.py
+++ main.py
@@ -44,11 +44,43 @@ def evbee_decode_pkt(pkt_data):
 
 def evbee_handle_cmd(decoded):
     global evbee_write_pkt
+
+    # 0x0001 = Response to 0x0000 initialise command. Send 0x0004 command to set the time
     if(decoded["cmd"] == 0x0001):
         cmd_data = bytearray(b'\x01\x30\x00\x00')
         unix_ts = int(time.time())
         cmd_data = cmd_data + unix_ts.to_bytes(4, 'little')
         evbee_write_pkt = evbee_build_pkt(0x0004, cmd_data)
+
+    # 0x0005 = Response to 0x0004 set time command. Send 0x00A4 command to get faults. This response contains the firmware version etc.
+    elif(decoded["cmd"] == 0x0005):
+        evbee_write_pkt = evbee_build_pkt(0x00A4, b'\x01\x00\x00\x00')
+
+    # 0x00A5 = Response to 0x00A4 get faults command. Send 0x00A6 to get currents. TODO: Figure out the format for the faults
+    elif(decoded["cmd"] == 0x00A5):
+        evbee_write_pkt = evbee_build_pkt(0x00A6, b'')
+
+    # 0x00A7 = Response to 0x00A6 get current command. Could do something with setting the current, but for now do nothing
+    elif(decoded["cmd"] == 0x00A7):
+        print("Currents, min = ", int.from_bytes(decoded["data"][0:2], 'little'), ", max = ", int.from_bytes(decoded["data"][2:4], 'little'))
+
+    # 0x0104 = Charger status update. Send 0x0105 to ACK the update and keep the time up to date
+    elif(decoded["cmd"] == 0x0104):
+        print("Staus update: Plug status = ", 
+              int(decoded["data"][1]), 
+              ", Voltage = ", 
+              int.from_bytes(decoded["data"][4:6], 'little') / 100.0, 
+              "V, Current = ", 
+              int.from_bytes(decoded["data"][6:8], 'little') / 100.0,
+              "A, Charge Time = ",
+              int.from_bytes(decoded["data"][8:12], 'little'),
+              "s, Energy = ",
+              int.from_bytes(decoded["data"][12:14], 'little') / 1000.0,
+              "kWh")
+        unix_ts = int(time.time())
+        evbee_write_pkt = evbee_build_pkt(0x0004, unix_ts.to_bytes(4, 'little'))
+    
+
 
 def notification_handler(characteristic: BleakGATTCharacteristic, data: bytearray):
     """Simple notification handler which prints the data received."""
